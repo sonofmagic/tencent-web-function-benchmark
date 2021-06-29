2b0d9618da098e75faccfe286ca915ddaa10c770
SonOfMagic <qq1324318532@gmail.com>
2021-06-30 00:05:52 +0800

    featL commit report

--- README.md
+++ README.md
@@ -6,9 +6,15 @@
 
 而原先基于 event 的 serverless components web [组件们](https://github.com/serverless-components/tencent-framework-components) "身世浮沉雨打萍"
 
-## 同样代码的四个案例
+## 同样代码的 8 个案例
 
-1. 使用 `serverless express` 部署的一个 event 类型 SCF
-2. 使用 `serverless scf` 部署的一个 web 类型 SCF
-3. 使用自定义镜像 node12 部署的一个 web 类型 SCF
-4. 使用自定义镜像 node14 部署的一个 web 类型 SCF
+1. 使用 `serverless express` 部署的一个 `node10.15` event 类型 SCF
+1. 使用 `serverless express` 部署的一个 `node12.16` event 类型 SCF
+1. 使用 `serverless scf` 部署的一个 `node10.15` web 类型 SCF
+1. 使用 `serverless scf` 部署的一个 `node12.16` web 类型 SCF
+1. 使用自定义镜像 node10 部署的一个 web 类型 SCF
+1. 使用自定义镜像 node12 部署的一个 web 类型 SCF
+1. 使用自定义镜像 node14 部署的一个 web 类型 SCF
+1. 使用自定义镜像 node16 部署的一个 web 类型 SCF
+
+预制并发实例都为 `1`，内存都为 `128MB`,压测代码都是相同的配置
--- benchmark/index.js
+++ benchmark/index.js
@@ -1,22 +1,44 @@
-const eventSCFUrl = 'https://service-lqpo8cnh-1257725330.sh.apigw.tencentcs.com'
+const eventSCFUrlNode10 =
+  'https://service-0lctndet-1257725330.sh.apigw.tencentcs.com'
+const eventSCFUrlNode12 =
+  'https://service-a7gfk9kz-1257725330.sh.apigw.tencentcs.com'
 
-const webTypeSCFUrl =
-  'https://service-00ohv7wp-1257725330.sh.apigw.tencentcs.com'
+const webTypeSCFUrlNode10 =
+  'https://service-mml88olr-1257725330.sh.apigw.tencentcs.com'
 
+const webTypeSCFUrlNode12 =
+  'https://service-nxfqhjs9-1257725330.sh.apigw.tencentcs.com'
+
+const webTypeCustomImageNode10 =
+  'https://service-0h09ks4l-1257725330.sh.apigw.tencentcs.com'
 const webTypeCustomImageNode12 =
   'https://service-94y9c3qh-1257725330.sh.apigw.tencentcs.com'
 
 const webTypeCustomImageNode14 =
   'https://service-h8xv1r41-1257725330.sh.apigw.tencentcs.com'
 
+const webTypeCustomImageNode16 =
+  'https://service-2jglye19-1257725330.sh.apigw.tencentcs.com/'
 const queue = [
   {
-    name: 'event',
-    url: eventSCFUrl
+    name: 'event-node10',
+    url: eventSCFUrlNode10
   },
   {
-    name: 'normal-web',
-    url: webTypeSCFUrl
+    name: 'event-node12',
+    url: eventSCFUrlNode12
+  },
+  {
+    name: 'normal-web-node10',
+    url: webTypeSCFUrlNode10
+  },
+  {
+    name: 'normal-web-node12',
+    url: webTypeSCFUrlNode12
+  },
+  {
+    name: 'custom-web-node10',
+    url: webTypeCustomImageNode10
   },
   {
     name: 'custom-web-node12',
@@ -25,6 +47,10 @@ const queue = [
   {
     name: 'custom-web-node14',
     url: webTypeCustomImageNode14
+  },
+  {
+    name: 'custom-web-node16',
+    url: webTypeCustomImageNode16
   }
 ]
 
